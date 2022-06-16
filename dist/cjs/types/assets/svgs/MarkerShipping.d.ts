425298505214e1eae28cf11333cc94e3ff3a9cf1
ChimaG <chimagee@mymusictaste.com>
2022-06-17 01:33:56 +0800

    fix: bundle

--- dist/cjs/types/assets/svgs/MarkerShipping.d.ts
+++ dist/cjs/types/assets/svgs/MarkerShipping.d.ts
@@ -3,6 +3,6 @@ export declare const ShippingMarker: ({ id, fill, selected, className, opacity, 
     id?: string | undefined;
     fill?: string | undefined;
     className?: string | undefined;
-    selected?: number | undefined;
+    selected?: boolean | undefined;
     opacity?: number | undefined;
 }) => JSX.Element;
